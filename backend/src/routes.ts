16e594a9ce147673d08fc7ba96d51490c0d28ca6
Rayan Wilbert <rayan.rmw@gmail.com>
2022-07-19 11:44:29 -0300

    Backend validation done :white_check_mark:

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -1,6 +1,7 @@
 import express from 'express';
 import multer from 'multer';
 import multerConfig from './config/multer';
+import { celebrate, Joi } from 'celebrate';
 
 import PointsControler from './controllers/PointsController';
 import ItemsController from './controllers/ItemsController';
@@ -12,6 +13,24 @@ const upload = multer(multerConfig);
 routes.get('/items', ItemsController.index);
 routes.get('/points', PointsControler.index);
 routes.get('/points/:id', PointsControler.show);
-routes.post('/points', upload.single('image'), PointsControler.store);
+routes.post(
+  '/points', 
+  upload.single('image'), 
+  celebrate({
+    body: Joi.object().keys({
+      name: Joi.string().required(),
+      email: Joi.string().required().email(),
+      whatsapp: Joi.number().required(),
+      latitude: Joi.number().required(),
+      longitude: Joi.number().required(),
+      city: Joi.string().required(),
+      uf: Joi.string().required().max(2),
+      items: Joi.string().required(),
+    }),
+  }, {
+    abortEarly: false,
+  }),
+  PointsControler.store
+);
 
 export default routes;
